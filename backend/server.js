1e7529829fbe806ee6eb668f4086b0856370ab40
Saurabh Pandey <123140260+mrsaurabh2312@users.noreply.github.com>
2025-07-14 16:47:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const orderRoutes = require('./routes/order');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/pooja-samagri', {
+mongoose.connect('mongodb+srv://balkishan0107sharma:hqlaExu58bxV6zAx@cluster0.vrfvxjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'));
 
